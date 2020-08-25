f5b8390f70ed94c73eef622d068cd42867ff91eb
Ramiro Contreras León <racramiro@gmail.com>
2020-08-25 12:44:32 -0500

    clase 7

---
+++ src/index.js
@@ -0,0 +1,41 @@
+// const element = document.createElement('h1');
+// element.innerText = 'Hello, Platzi Badges!';
+
+// const container = document.getElementById('app');
+
+// container.appendChild(element);
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+// const jsx = <h1>Hello, Platzi-Badges!</h1>;
+
+// const element = React.createElement("h1", {}, "Hola, soy un children!");
+// const element = React.createElement(
+//   "a",
+//   { href: "https://leonsonidovirtual.com/" },
+//   "Ir a LSV"
+// );
+const name = "Mortal";
+// const element = React.createElement("h1", {}, `Hola soy ${name}`);
+
+// const jsx = <h1> Hola soy, {name}</h1>;
+
+const jsx = (
+  <div>
+    <h1>Hola soy ramiro</h1>
+    <p>Soy inge</p>
+  </div>
+);
+
+// const element = React.createElement(
+//   "div",
+//   {},
+//   React.createElement("h1", {}, "haisuasdhuasd"),
+//   React.createElement("p", {}, "haisuasdhuasd")
+// );
+
+const container = document.getElementById("app");
+
+// ReactDOM.render(__qué__, __dónde__);
+ReactDOM.render(jsx, container);
