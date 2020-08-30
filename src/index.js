d4561aa4c8db014bcc0adef92b0fc38d498d0084
Ramiro Contreras León <racramiro@gmail.com>
2020-08-29 23:01:43 -0500

    Proyecto terminado

--- src/index.js
+++ src/index.js
@@ -8,6 +8,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import "bootstrap/dist/css/bootstrap.css";
+import "./global.css";
+
+import App from "./components/App";
+
 // const jsx = <h1>Hello, Platzi-Badges!</h1>;
 
 // const element = React.createElement("h1", {}, "Hola, soy un children!");
@@ -16,26 +21,29 @@ import ReactDOM from "react-dom";
 //   { href: "https://leonsonidovirtual.com/" },
 //   "Ir a LSV"
 // );
-const name = "Mortal";
+// const name = "Mortal";
 // const element = React.createElement("h1", {}, `Hola soy ${name}`);
 
 // const jsx = <h1> Hola soy, {name}</h1>;
 
-const jsx = (
-  <div>
-    <h1>Hola soy ramiro</h1>
-    <p>Soy inge</p>
-  </div>
-);
-
-// const element = React.createElement(
-//   "div",
-//   {},
-//   React.createElement("h1", {}, "haisuasdhuasd"),
-//   React.createElement("p", {}, "haisuasdhuasd")
+// const jsx = (
+//   <div>
+//     <h1>Hola soy ramiro</h1>
+//     <p>Soy inge</p>
+//   </div>
 // );
 
 const container = document.getElementById("app");
 
+ReactDOM.render(<App />, container);
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(jsx, container);
+// ReactDOM.render(
+//   <Badge
+//     firstName="León"
+//     lastName="Contreras"
+//     avatarUrl="https://s.gravatar.com/avatar/ee292d382b296908326f83a68e308e1e?s=80"
+//     jobTittle="Full Stack"
+//     twitter="ramirocontrerasleon"
+//   />,
+//   container
+// );
